ca063c6181416fa91cbea63b188e2c348eebd27d
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-04-24 16:51:37 +0700

    [master] Food and Snake

---
+++ script.js
@@ -0,0 +1,18 @@
+const playBoard = document.querySelector('.play_board');
+
+let foodX, foodY;
+let snakeX = 5, snakeY = 10;
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX};"></div>`;
+    htmlMarkup += `<div class="snake_head" style="grid-area: ${snakeY} / ${snakeX};"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+}
+
+changeFoodPosition();
+initGame();
